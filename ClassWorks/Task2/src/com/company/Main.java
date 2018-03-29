ab13f4c473d088b21e5340f3825ad046b807d6e5
Student5 <Student5@itpark.ru>
2018-03-29 20:39:20 +0300

    fix ignore

---
+++ ClassWorks/Task2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+class Task2 {
+
+    public static void main(String[] args) {
+        // write your code here
+        ArrayList<Integer> list = new ArrayList<Integer>();
+    }
+}
+
+//int[] intArray = { 1, 2, 3, 4, 5 };
+//int[] removed = ArrayUtils.removeElement(intArray, 3);//create a new array
+//System.out.println(Arrays.toString(removed));
